3de8541aa0a49a0c7c8524254fb607ea02ee3893
inaba1115 <inaba1115@gmail.com>
2024-10-06 21:38:50 +0900

    add play

--- symbolism/__init__.py
+++ symbolism/__init__.py
@@ -2,10 +2,13 @@ from .buffer import Buffer
 from .note import Note
 from .scale import Scale
 from .scales import scales
+from .utils import get_output_names, play
 
 __all__ = [
     "Buffer",
     "Note",
     "Scale",
     "scales",
+    "get_output_names",
+    "play",
 ]
--- symbolism/utils.py
+++ symbolism/utils.py
@@ -3,3 +3,14 @@ import mido  # type: ignore
 
 def sec_to_tick(sec: float) -> int:
     return mido.second2tick(sec, ticks_per_beat=480, tempo=500000)
+
+
+def get_output_names() -> list[str]:
+    return mido.get_output_names()
+
+
+def play(port: str, fname: str) -> None:
+    with mido.open_output(port) as outport:
+        mid = mido.MidiFile(fname)
+        for msg in mid.play():
+            outport.send(msg)
