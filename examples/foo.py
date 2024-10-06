4f94dd99980efab05197374116ccecb65d3fe565
inaba1115 <inaba1115@gmail.com>
2024-10-06 22:17:46 +0900

    impl play

--- examples/foo.py
+++ examples/foo.py
@@ -4,15 +4,27 @@ import symbolism
 def main():
     buffer = symbolism.Buffer()
     scale = symbolism.Scale(0, symbolism.scales["major"])
-    dt = 1
+
+    score = [
+        ([0, 2, 4, 6], 1),
+        ([], 0.5),
+        ([2], 0.3),
+        ([3], 0.3),
+        ([4], 0.3),
+        ([7], 0.3),
+        ([-1], 0.3),
+        ([0], 0.3),
+        ([0, 2, 4, 6], 1),
+    ]
 
     t = 0
-    for x in [0, 1, 2, 2, 0, 4, 5, 6]:
-        buffer.add(t, symbolism.Note(scale.map(x) + 60, 100, dt))
+    for notes, dt in score:
+        for note in notes:
+            buffer.add(t, symbolism.Note(scale.map(note) + 60, 100, dt))
         t += dt
 
-    buffer.write("foo.mid")
-    symbolism.play(symbolism.get_output_names()[0], "foo.mid")
+    # buffer.write("foo.mid")
+    buffer.play(symbolism.get_output_names()[0])
 
 
 if __name__ == "__main__":
--- symbolism/utils.py
+++ symbolism/utils.py
@@ -5,12 +5,9 @@ def sec_to_tick(sec: float) -> int:
     return mido.second2tick(sec, ticks_per_beat=480, tempo=500000)
 
 
+def tick_to_sec(tick: int) -> float:
+    return mido.tick2second(tick, ticks_per_beat=480, tempo=500000)
+
+
 def get_output_names() -> list[str]:
     return mido.get_output_names()
-
-
-def play(port: str, fname: str) -> None:
-    with mido.open_output(port) as outport:
-        mid = mido.MidiFile(fname)
-        for msg in mid.play():
-            outport.send(msg)
